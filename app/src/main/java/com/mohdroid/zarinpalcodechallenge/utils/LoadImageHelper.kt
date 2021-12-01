0de1677f09281e5fc68f594a0716eecc8f91b93b
mohsenmirtalebi <Mohsen.mirtalebi@gmail.com>
2021-12-01 10:31:57 +0330

    Add helper for load and show images

---
+++ app/src/main/java/com/mohdroid/zarinpalcodechallenge/utils/LoadImageHelper.kt
@@ -0,0 +1,37 @@
+package com.mohdroid.zarinpalcodechallenge.utils
+
+import android.content.Context
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
+import javax.inject.Inject
+import javax.inject.Singleton
+
+interface LoadImageHelper {
+
+    fun loadImage(url: String, view: ImageView)
+
+}
+
+@Singleton
+class LoadImageHelperImpl @Inject constructor(ctx: Context) :
+    LoadImageHelper {
+    private val glide: RequestManager = Glide.with(ctx)
+
+    override fun loadImage(url: String, view: ImageView) {
+        handleLoadImage {
+            glide
+                .load(url)
+                .into(view)
+        }
+    }
+
+    private fun handleLoadImage(loadImage: () -> Unit) {
+        try {
+            loadImage()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+}
